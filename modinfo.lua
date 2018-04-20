c7ae9033fc3b239a4acc282f62e013c7b8eee342
cr4shmaster <cr4shmaster@hotmail.com>
2018-04-20 14:25:27 +0300

    minow tweaks

--- modinfo.lua
+++ modinfo.lua
@@ -12,46 +12,34 @@ client_only_mod = false
 icon_atlas = "modicon.xml"
 icon = "modicon.tex"
 
-
-local crsToggle = {
-    {description = "Yes", data = true},
-    {description = "No", data = false},
-}
-
-local crsIngredient = {
-    {description = "1", data = 1},
-    {description = "2", data = 2},
-    {description = "3", data = 3},
-    {description = "4", data = 4},
-    {description = "5", data = 5},
-    {description = "6", data = 6},
-    {description = "7", data = 7},
-    {description = "8", data = 8},
-    {description = "9", data = 9},
-    {description = "10", data = 10},
-    {description = "15", data = 15},
-    {description = "20", data = 20},
-}
+local function crsSetCount(k)
+    return {description = ""..k.."", data = k}
+end
 
 local function rawIngredients(name, desc)
     local ext = desc ~= nil and desc or name
-    return{
-        name = "cfgRaw"..name,
-        label = "Raw "..ext,
-        options = crsIngredient,
-        default = 10,
-    }
+    return {name = "cfgRaw"..name, label = "Raw "..ext, options = crsIngredient, default = 10,}
 end
 
 local function cookedIngredients(name, desc)
     local ext = desc ~= nil and desc or name
-    return{
-        name = "cfgCooked"..name,
-        label = ""..ext.." from can",
-        options = crsIngredient,
-        default = 7,
-    }
+    return {name = "cfgCooked"..name, label = ""..ext.." from can", options = crsIngredient, default = 7,}
 end
+
+local function crsSetTab(k)
+    local name = {"Tools", "Survival", "Farm", "Science", "Structures", "Refine", "Magic"}
+    return {description = ""..name[k].."", data = k}
+end
+
+local function crsSetTech(k)
+    local name = {"None", "Science Machine", "Alchemy Engine", "Prestihatitator", "Shadow Manip.", "Broken APS", "Repaired APS"}
+    return {description = ""..name[k].."", data = k}
+end
+
+local crsTab = {} for k=1,7,1 do crsTab[k] = crsSetTab(k) end
+local crsTech = {} for k=1,7,1 do crsTech[k] = crsSetTech(k) end
+local crsIngredient = {} for k=1,20,1 do crsIngredient[k] = crsSetCount(k) end
+local crsToggle = {{description = "Yes", data = true}, {description = "No", data = false},}
 
 local crsPrefabs = {
     {"Bananas"},
@@ -95,73 +83,21 @@ local mfrPrefabs = { -- More Fruits
 }
 
 local function crsDivide(name, title)
-    return {   
-        name = "cfg"..name.."Title",
-        label = title,
-        options = {{description = "", data = false},},
-        default = false,
-    }
+    return {name = "cfg"..name.."Title", label = title, options = {{description = "", data = false},}, default = false,}
 end
 
 configuration_options = {
     crsDivide("CNF", "Canned Food Settings"),
-    {
-        name = "cfgRecipeTab",
-        label = "Recipe Tab",
-        options = {
-            {description = "Tools", data = 1},
-            {description = "Survival", data = 2},
-            {description = "Farm", data = 3},
-            {description = "Science", data = 4},
-            {description = "Structures", data = 5},
-            {description = "Refine", data = 6},
-            {description = "Magic", data = 7},
-        },
-        default = 6,
-    },
-    {
-        name = "cfgRecipeTech",
-        label = "Recipe Tech",
-        options = {
-            {description = "None", data = 1},
-            {description = "Science Machine", data = 2},
-            {description = "Alchemy Engine", data = 3},
-            {description = "Prestihatitator", data = 4},
-            {description = "Shadow Manip.", data = 5},
-            {description = "Broken APS", data = 6},
-            {description = "Repaired APS", data = 7},
-            {description = "Obs. Workbench", data = 8},
-        },
-        default = 2,
-    },
-    {
-        name = "cfgAddHoney",
-        label = "Honey Required",
-        options = crsToggle,
-        default = true,
-    },
-    {
-        name = "cfgHoney",
-        label = "How Much Honey",
-        options = crsIngredient,
-        default = 10,
-    }, 
-    {
-        name = "cfgAddNitre",
-        label = "Nitre Required",
-        options = crsToggle,
-        default = true,
-    },
-    {
-        name = "cfgNitre",
-        label = "How Much Nitre",
-        options = crsIngredient,
-        default = 1,
-    }, 
+    {name = "cfgRecipeTab", label = "Recipe Tab", options = crsTab, default = 6,},
+    {name = "cfgRecipeTech", label = "Recipe Tech", options = crsTech, default = 2,},
+    {name = "cfgAddHoney", label = "Honey Required", options = crsToggle, default = true,},
+    {name = "cfgHoney", label = "How Much Honey", options = crsIngredient, default = 10,}, 
+    {name = "cfgAddNitre", label = "Nitre Required", options = crsToggle, default = true,},
+    {name = "cfgNitre", label = "How Much Nitre", options = crsIngredient, default = 1,}, 
 }
 
 local function crsAddOptions(table)
-    for k=1, #table, 1 do -- raw and cooked ingredients options
+    for k=1, #table, 1 do -- raw and cooked ingredients
         desc = table[k][2] ~= nil
         configuration_options[#configuration_options+1] = rawIngredients(table[k][1], desc and table[k][2])
         configuration_options[#configuration_options+1] = cookedIngredients(table[k][1], desc and table[k][2])
